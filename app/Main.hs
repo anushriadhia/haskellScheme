64a0647cb38d0a9d9e57f4bff345c765674e2a68
Anushri Adhia <anushri.adhia@gmail.com>
2019-05-28 12:23:48 -0400

    expanded parseNumber, cleaned up parseString

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
+import Numeric
 
 data LispVal = Atom String
              | List [LispVal]
@@ -9,48 +10,79 @@ data LispVal = Atom String
              | Number Integer 
              | String String 
              | Bool Bool
+             | Character Char
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<+<?@^_~"
+symbol = oneOf "!$%&|*+-/:<+<?@^_~"
 
 parseString :: Parser LispVal
 parseString = do 
                 char '"'
-                x <- many chars
+                x <- many (noneOf "\"\\" <|> escapedChars)
                 char '"'
                 return $ String x
-    where chars = noneOf "\"" <|> escaped
-          escaped = char '\\' >> choice (zipWith escapedChar codes replacements)
-          escapedChar :: Char -> Char -> Parser Char
-          escapedChar code replacement = char code >> return replacement
-          codes = ['b', 'n', 'f', 'r', 't', '\\', '\"', '/']
-          replacements = ['\b', '\n', '\f', '\r', '\t', '\\', '\"', '/']
+
+escapedChars :: Parser Char
+escapedChars = do
+                char '\\'
+                x <- oneOf "\\\"nrt" 
+                return $ case x of
+                           '\\' -> x
+                           '"' -> x
+                           'n' -> '\n'
+                           'r' -> '\r'
+                           't' -> '\t'
 
 parseAtom :: Parser LispVal
 parseAtom = do
               first <- letter <|> symbol
               rest <- many (letter <|> digit <|> symbol)
               let atom = first:rest
-              return $ case atom of
-                         "#t" -> Bool True
-                         "#f" -> Bool False
-                         _    -> Atom atom
+              return $ Atom atom
+
+parseBool :: Parser LispVal
+parseBool = do
+              char '#'
+              ( char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
 
 parseNumber :: Parser LispVal
-  
-parseNumber = liftM (Number . read) $ many1 digit
--- parseNumber = do 
---                 stringNum <- many1 digit
---                 return $ (Number . read) stringNum 
+parseNumber = parseDecimal1 <|> parseDecimal2 <|> parseHex <|> parseOct <|> parseBin
 
--- parseNumber = many1 digit >>= \numString -> Number (read numString)
--- figure out how to do the bind ^^^^
+parseDecimal1 :: Parser LispVal
+parseDecimal1 = Number . read <$> many1 digit 
 
+parseDecimal2 :: Parser LispVal
+parseDecimal2 = do try $ string "#d"
+                   x <- many1 digit
+                   (return . Number . read) x
+
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+              x <- many1 hexDigit
+              return $ Number (hex2dig x)
+
+parseOct :: Parser LispVal
+parseOct = do try $ string "#o"
+              x <- many1 octDigit
+              return $ Number (oct2dig x)
+
+parseBin :: Parser LispVal
+parseBin = do try $ string "#b"
+              x <- many1 (oneOf "10")
+              return $ Number (bin2dig x)
+
+oct2dig x = fst $ head (readOct x) 
+hex2dig x = fst $ head (readHex x) 
+bin2dig  = bin2dig' 0
+bin2dig' digint "" = digint
+bin2dig' digint (x:xs) = let old = 2 * digint + (if x == '0' then 0 else 1) in
+                         bin2dig' old xs
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
+        <|> parseBool
 
 spaces :: Parser ()
 spaces = skipMany1 space
